3e6078b87347053e7056ab55f11965029de6e043
Surbhi Patel <patel200123@gmail.com>
2026-01-31 15:09:22 +0530

    Supabase SetUp

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rkkmyjspddpieefamyem.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJra215anNwZGRwaWVlZmFteWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDc2MDQsImV4cCI6MjA4NDk4MzYwNH0.mL2e9cSd-BEeT4-4aOeBH_YAW5jWyfFqH0sj_jD4mTY'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
